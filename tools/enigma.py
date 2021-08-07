c227d8c5308376d0f2ccd0b88d45b91f3864aed9
Christian Berger <christian@clarke-3.de>
2021-08-07 16:45:34 +0200

    fix encoding

---
+++ tools/enigma.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3 
+
+import xml.etree.ElementTree as ET
+import os
+import time
+import requests
+
+outdir="outdir"
+
+req=requests.get("http://192.168.5.12/web/getservices?sRef=1:7:1:0:0:0:0:0:0:0:FROM BOUQUET \"userbouquet.SD.tv\" ORDER BY bouquet")
+req.encoding="UTF-8"
+
+#request.get(URL, auth=('user','pass'))
+
+
+#tree=ET.parse("test.xml")
+tree=ET.fromstring(req.text)
+
+root=tree
+
+#.getroot()
+
+for child in root:
+    chname=child[1].text.strip().replace(" ","_").replace("/","_")
+    print(chname)
+    os.system("wget -o /dev/null -O - http://192.168.5.12:8001/"+child[0].text+" | ../src/ts_teletext --ts --stop")
+    with os.scandir(".") as it:
+        for entry in it:
+            if entry.name.startswith("0x") and entry.is_file:
+                os.rename(entry.name,outdir+"/"+chname+" "+time.strftime("%Y-%m-%d_%H-%M-%S", time.gmtime())+" "+entry.name);
+
