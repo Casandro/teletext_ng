102a1116432caf7787b409668467f5d695e53d2e
root <root@vdr.lan>
2023-07-26 20:34:28 +0200

    Minor changes

--- tools/dvbv5/scan_teletext.sh
+++ tools/dvbv5/scan_teletext.sh
@@ -4,19 +4,19 @@
 
 channels=`mktemp /tmp/XXXXXX.channels`
 
-./dvbv5-transponders_from_sql.pl > $channels
-
 OUTDIR=data
 
 while true
 do
 
-grep "^\[[0-9]*\]" $channels | while IFS= read -r line; do
+./dvbv5-transponders_from_sql.pl > $channels
+grep "^\[[0-9]*\]" $channels | sort -R | while IFS= read -r line; do
 	mux=`echo $line | tr -d "[]"`
-	echo $mux
+	echo "Transponder: $mux"
 	mkdir -p $OUTDIR/$mux
 	date >> $OUTDIR/$mux/log
 	dvbv5-zap -t 3600 -c $channels -w-1 -a0 -W 250 -f1 -P -o - "$mux" | ../../src/ts_teletext --ts --stop #| tee -a $OUTDIR/$mux/log
+	echo "Vorheriger Transponder: $mux"
 	dt=`date -u -Iseconds`
 	date >> $OUTDIR/$mux/log
 	echo >> $OUTDIR/$mux/log
@@ -29,6 +29,10 @@ grep "^\[[0-9]*\]" $channels | while IFS= read -r line; do
 		fi
 	done
 	./tta_to_services.pl &
+	if [ -f move_to_server.sh ] 
+	then
+		./move_to_server.sh
+	fi
 done
 sleep 10
 done
