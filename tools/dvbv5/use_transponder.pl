615ca30e5db514078f240a621f04711a0c6f681e
Christian Berger <christian@clarke-3.de>
2023-07-31 23:55:32 +0200

    Improved handling for unreliable transponders

--- tools/dvbv5/use_transponder.pl
+++ tools/dvbv5/use_transponder.pl
@@ -9,19 +9,19 @@ my $worker=$ARGV[0];
 
 my $task=$ARGV[1];
 
+my $dbh=DBI->connect("DBI:MariaDB:teletext",'teletext','teletext');
+die "failed to connect to MySQL database:DBI->errstr()" unless($dbh);
 if ($task eq "GET") {
-	my $dbh=DBI->connect("DBI:MariaDB:teletext",'teletext','teletext');
-	die "failed to connect to MySQL database:DBI->errstr()" unless($dbh);
+
 	
-	my $q=$dbh->prepare("SELECT id FROM transponders WHERE (in_use_by<0 OR in_use_by=?) AND weight >RAND()*90 ORDER BY date_add(last_used, INTERVAL RAND()*3600 SECOND) LIMIT 1");
+	my $q=$dbh->prepare("SELECT id FROM transponders WHERE (in_use_by<0 OR in_use_by=?) AND weight > RAND()*90 AND weight>0 ORDER BY date_add(last_used, INTERVAL RAND()*3600 SECOND) LIMIT 1"); 
 	$q->execute($worker);
 	my ($id)=$q->fetchrow_array();
 	$dbh->do("UPDATE transponders set in_use_by=? WHERE id=?", undef, $worker, $id);
 	print $id;
-} else {
-	my $transponder=$ARGV[1];
-	my $dbh=DBI->connect("DBI:MariaDB:teletext",'teletext','teletext');
-	die "failed to connect to MySQL database:DBI->errstr()" unless($dbh);
+} if ($task eq "ERROR") {
+	$dbh->do("UPDATE transponders set in_use_by=-1, weight=weight-10 WHERE in_use_by=?", undef, $worker);
 	
-	$dbh->do("UPDATE transponders set in_use_by=-1, last_used=NOW() WHERE id=?", undef, $transponder);
+}else {
+	$dbh->do("UPDATE transponders set in_use_by=-1, last_used=NOW() WHERE in_use_by=?", undef, $worker);
 }
