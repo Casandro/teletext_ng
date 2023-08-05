24181d40e129af480283cee25ba80455e7b99481
Christian Berger <christian@clarke-3.de>
2023-08-05 12:00:14 +0200

    Modify transponder weight on success

--- tools/dvbv5/use_transponder.pl
+++ tools/dvbv5/use_transponder.pl
@@ -11,10 +11,9 @@ my $task=$ARGV[1];
 
 my $dbh=DBI->connect("DBI:MariaDB:teletext",'teletext','teletext');
 die "failed to connect to MySQL database:DBI->errstr()" unless($dbh);
-if ($task eq "GET") {
 
-	
-	my $q=$dbh->prepare("SELECT id FROM transponders WHERE (in_use_by<0 OR in_use_by=?) AND weight > RAND()*90 AND weight>0 ORDER BY date_add(last_used, INTERVAL RAND()*3600 SECOND) LIMIT 1"); 
+if ($task eq "GET") {
+	my $q=$dbh->prepare("SELECT id FROM transponders WHERE (in_use_by<0 OR in_use_by=?) AND weight > RAND()*50+40 AND weight>0 ORDER BY date_add(last_used, INTERVAL RAND()*3600 SECOND) LIMIT 1"); 
 	$q->execute($worker);
 	my ($id)=$q->fetchrow_array();
 	$dbh->do("UPDATE transponders set in_use_by=? WHERE id=?", undef, $worker, $id);
@@ -23,5 +22,5 @@ if ($task eq "GET") {
 	$dbh->do("UPDATE transponders set in_use_by=-1, weight=weight-10 WHERE in_use_by=?", undef, $worker);
 	
 }else {
-	$dbh->do("UPDATE transponders set in_use_by=-1, last_used=NOW() WHERE in_use_by=?", undef, $worker);
+	$dbh->do("UPDATE transponders set in_use_by=-1, last_used=NOW(), weight=weight+1 WHERE in_use_by=?", undef, $worker);
 }
