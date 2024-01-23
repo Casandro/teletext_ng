4ff91fc88b165a62493be28c2fac2495b14e6962
Christian Berger <github@clarke-3.de>
2024-01-23 22:37:02 +0100

    Support for environment variables and locking of muxes

--- tools/tvheadend/README.md
+++ tools/tvheadend/README.md
@@ -3,6 +3,19 @@
 To use this you need a TV-Headend installation with a user with `admin` and `web-interface` privileges. Please make sure to limit the user to just the host you run this program on.
 
 
+## configuration variables
+
+The program is configured via environment variables:
+
+  * `TVHEADEND_IP` The IP-Address of the TV-Headend. default `127.0.0.1`
+  * `TVHEADEND_PORT` The Port of the TV-Headend. default `9981` 
+  * `TVHEADEND_USER` the user. default `teletext`
+  * `TVHEADEND_PASS` the password, default `teletext`
+  * `OUTDIR` the directory for the outgoing files, default `outdir`
+  * `TMPDIR` the temporary directory for collecting data, default `/tmp/`
+  * `LOCKDIR` the directory to store locks in, default `lock` 
+
+
 ## translations.json
 
 This file defines translations between simplified service names and text service names
--- tools/tvheadend/tvheadend.py
+++ tools/tvheadend/tvheadend.py
@@ -6,11 +6,65 @@ import requests
 from requests.auth import HTTPDigestAuth
 import json
 import datetime
+import time
+
+
+tvheadend_ip="127.0.0.1"
+tvheadend_port="9981"
+tvheadend_user="teletext"
+tvheadend_pass="teletext"
+
+if "TVHEADEND_IP" in os.environ:
+    tvheadend_ip=os.environ["TVHEADEND_IP"]
+
+if "TVHEADEND_PORT" in os.environ:
+    tvheadend_ip=os.environ["TVHEADEND_PORT"]
+
+if "TVHEADEND_USER" in os.environ:
+    tvheadend_ip=os.environ["TVHEADEND_USER"]
+
+if "TVHEADEND_PASS" in os.environ:
+    tvheadend_ip=os.environ["TVHEADEND_PASS"]
 
 outdir="outdir"
+if "OUTDIR" in os.environ:
+    outdir=os.environ["OUTDIR"]
 
-base_url="http://127.0.0.1:9981/"
-base_url_auth="http://teletext:teletext@127.0.0.1:9981/"
+tmpdir="/tmp/"
+if "TMPDIR" in os.environ:
+    outdir=os.environ["TMPDIR"]
+
+lockdir="lock"
+if "LOCKDIR" in os.environ:
+    lockdir=os.environ["LOCKDIR"]
+os.makedirs(lockdir, exist_ok=True)
+
+
+def clean_locks():
+    with os.scandir(lockdir) as it:
+        for f in it:
+            if f.is_file():
+                mtime=f.stat().st_mtime
+                age=time.time()-mtime
+                if age>7200:
+                    os.remove(f.path)
+                print(f.name, age)
+
+def get_lock(muxname):
+    clean_locks()
+    if os.path.exists(lockdir+"/"+muxname):
+        return False
+    f=open(lockdir+"/"+muxname, "w")
+    f.write("XXXXX")
+    f.close()
+    return True
+
+def remove_lock(muxname):
+    os.remove(lockdir+"/"+muxname)
+
+
+base_url="http://"+tvheadend_ip+":"+tvheadend_port+"/"
+base_url_auth="http://"+tvheadend_user+":"+tvheadend_pass+"@"+tvheadend_ip+":"+tvheadend_port+"/"
 
 url=base_url+"api/raw/export?class=dvb_mux"
 
@@ -35,8 +89,11 @@ all_mux_pids={}
 for mux in muxes:
     mux_pids=[]
     mux_uuid=mux["uuid"]
+    clean_locks()
 
     if mux['enabled']!=0:
+        if not get_lock(mux_uuid):
+            continue
         for service in mux['services']:
             req=requests.get(base_url+"api/raw/export?uuid="+service, auth=HTTPDigestAuth('teletext', 'teletext'))
             channel=json.loads(req.text)
@@ -66,11 +123,11 @@ for mux in muxes:
                 if len(pids)>0:
                     pids=pids+","
                 pids=pids+str(stream[1]);
-            url=base_url_auth+"stream/mux/"+mux['uuid']+"?pids="+pids
+            url=base_url_auth+"stream/mux/"+mux_uuid+"?pids="+pids
             print(url)
-            out_tmp=outdir+"/"+mux['uuid']
+            out_tmp=tmpdir+"/"+mux_uuid
             os.makedirs(out_tmp, exist_ok=True)
-            date_prefix=datetime.datetime.now().astimezone().replace(microsecond=0).isoformat()
+            date_prefix=datetime.datetime.now().utcnow().isoformat(timespec="seconds")+"+00:00"
             os.system("timeout 7200 wget -o /dev/null -O - "+url+" | ../../src/ts_teletext --ts --stop -p"+out_tmp+"/"+date_prefix+"-")
             files=os.listdir(out_tmp)
             for service in mux_pids:
