c97737604c057cca81d1b4be6fdbe6447e8ee21c
Christian Berger <christian@clarke-3.de>
2024-01-25 20:17:22 +0100

    Fix bug where output was going to stdout instead of file

--- src/status_output.c
+++ src/status_output.c
@@ -7,60 +7,6 @@
 int so_current_line=0;
 int so_service_count=0;
 
-void so_move_to_line(const int line)
-{
-	if (line<so_current_line) { //Move upwards
-		printf("\x1b[%dA", so_current_line-line);
-	}
-	if (line>so_current_line) { //Move downwards
-		printf("\x1b[%dB", line-so_current_line);
-	}
-	printf("\r");
-	so_current_line=line;
-}
-
-void so_erase_line(const int line)
-{
-	so_move_to_line(line);
-	printf("\x1b[J");
-}
-
-int so_new_service()
-{
-	so_service_count=so_service_count+1;
-	so_move_to_line(so_service_count*SO_LINES_PER_TS);
-	for (int n=0; n<SO_LINES_PER_TS; n++) {
-		printf("\n");
-		so_current_line=so_current_line+1;
-	}
-	return so_service_count;
-}
-
-void so_move_to_position(all_pages_t *ap, const int p)
-{
-	if (ap==NULL) return;
-	if (ap->status_line<0) {
-		ap->status_line=so_new_service();
-	}
-	so_move_to_line(ap->status_line*SO_LINES_PER_TS+p);
-}
-
-void so_end_line(all_pages_t *ap, const int p)
-{
-	if (ap==NULL) return;
-	if (p<0) return;
-	printf("\x1b[K");
-	fflush(stdout);
-}
-
-void so_end()
-{
-	if (so_service_count<=0) return;
-	so_service_count=so_service_count+1;
-	so_move_to_line(so_service_count*SO_LINES_PER_TS);
-	printf("\n");
-	so_service_count=0;
-}
 
 void print_magstate(const all_pages_t *ap, const int magno, FILE *f)
 {
@@ -174,7 +120,7 @@ void print_missing_page_headers(const all_pages_t *ap, FILE *f, const char *eol)
 			fprintf(f, " Subpages: ");
 			for (int subpage=min_subpage; subpage<=max_subpage; subpage++) {
 				if (ap->pages[mainpage]->subpages[subpage]==NULL) {
-					printf(".");
+					fprintf(f,".");
 				} else {
 					int cnt=ap->pages[mainpage]->subpages[subpage]->cnt;
 					if (cnt<10) fprintf(f, "%c", cnt+'0'); else fprintf(f, "X");
@@ -198,17 +144,17 @@ int decode_mbcd(const uint8_t x)
 
 
 
-void print_bsdp(const uint8_t *packet)
+void print_bsdp(const uint8_t *packet, FILE *f)
 {
 	int mpag=de_hamm8_8(packet);
 	if (mpag<0) return;
 	int magazine=mpag&0x07;
 	int row=mpag>>3;
-	printf("%d %d ", magazine, row);
+	fprintf(f,"%d %d ", magazine, row);
 	if (magazine!=0) return;
 	if (row!=30) return;
 	int dc=de_hamm8(packet[2]);
-	printf("DC: %d ", dc);
+	fprintf(f,"DC: %d ", dc);
 	int ini_page_unit=de_hamm8(packet[3]);
 	int ini_page_tens=de_hamm8(packet[4]);
 	int subcode_1=de_hamm8(packet[5]);
@@ -218,19 +164,19 @@ void print_bsdp(const uint8_t *packet)
 	int ini_mag=(subcode_4>>1 & 0xe) | (subcode_2>>3);
 	int ini_page=ini_mag<<8 | ini_page_tens<<4 | ini_page_unit;
 	int ini_subpage=(subcode_4<<12 | subcode_3<<8 | subcode_2<<4 | subcode_1) & 0x3f7f;
-	printf("IP: %03x-%04x ", ini_page, ini_subpage);
+	fprintf(f,"IP: %03x-%04x ", ini_page, ini_subpage);
 	int network_identification_code=de_hamm8(packet[9])<<8 | de_hamm8(packet[10]);
-	printf("NICe: %04x ", network_identification_code);
+	fprintf(f,"NICe: %04x ", network_identification_code);
 	int time_offset_code=packet[11];
 	double offset=(time_offset_code & 0x3e)*0.25;
 	if ((time_offset_code&0x40)!=0) offset=-offset;
-	printf("TOC: %02x, %lf hours ", time_offset_code, offset);
+	fprintf(f,"TOC: %02x, %lf hours ", time_offset_code, offset);
 	int mjd=decode_mbcd(packet[12]+0x10)<<16 | decode_mbcd(packet[13])<<8 | decode_mbcd(packet[14]);
-	printf("MJD: %05x ", mjd);
-	printf("UTC: %02x:%02x:%02x ", decode_mbcd(packet[15]), decode_mbcd(packet[16]), decode_mbcd(packet[17]));
-	printf("Status Display: '");
-	for(int n=22; n<42; n++) printf("%c", packet[n]&0x7f);
-	printf("' ");
+	fprintf(f,"MJD: %05x ", mjd);
+	fprintf(f,"UTC: %02x:%02x:%02x ", decode_mbcd(packet[15]), decode_mbcd(packet[16]), decode_mbcd(packet[17]));
+	fprintf(f,"Status Display: '");
+	for(int n=22; n<42; n++) fprintf(f,"%c", packet[n]&0x7f);
+	fprintf(f,"' ");
 	
 
 }
@@ -240,7 +186,7 @@ void print_service_status(const all_pages_t *ap, const int pes, FILE *f, const c
 	//First line
 	fprintf(f, "Pid: 0x%04x, %03x-%s ", pes, ap->last_pageno, ap->last_header);
 	fprintf(f, "  %d ", ap->bsdp_cnt);
-	print_bsdp(ap->last_bsdp);
+	print_bsdp(ap->last_bsdp, f);
 	fprintf(f, "%s", eol);
 	print_missing_pages(ap, f);
 	fprintf(f,"%s",eol);
--- src/status_output.h
+++ src/status_output.h
@@ -1,7 +1,4 @@
 #pragma once
 
 #include "page.h"
-//void so_move_to_position(all_pages_t *ap, const int p);
-//void so_end_line(all_pages_t *ap, const int p);
-//void so_end();
 void print_service_status(const all_pages_t *ap, const int pes, FILE *f, const char *eol);
