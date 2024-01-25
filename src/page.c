fed52ccc8e875f7266fe9c2eadee12b3af9d26dc
Christian Berger <christian@clarke-3.de>
2024-01-25 20:10:44 +0100

    Handle small services better

--- src/page.c
+++ src/page.c
@@ -233,7 +233,7 @@ int allpages_done(all_pages_t *p)
 	int expected=0;
 	int count=0;
 	allpages_done_fraction(p, &expected, &count);
-	if (count==1) { //Special handling for empty services
+	if (count<10) { //Special handling for empty services
 		return 2;
 	}
 	if  ( (count==expected) && (tdiff>60*2) ) return 2; //If we think we have all pages, quit after a minute
