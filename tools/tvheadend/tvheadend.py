6aba6f739fc9d8177a7d08ba01e46fcf550ca4f1
Christian Berger <christian@clarke-3.de>
2024-02-10 14:52:22 +0100

    Big refactor in determining the order of muxes to scan

--- tools/tvheadend/tvheadend.py
+++ tools/tvheadend/tvheadend.py
@@ -87,7 +87,7 @@ def clean_locks():
                 age=time.time()-mtime
                 if age>7200:
                     os.remove(f.path)
-                    print("Removed statle lock for", f.name, age)
+                    log("Removed stale lock for", f.name, age)
 
 def get_lock(muxname):
     clean_locks()
@@ -114,21 +114,23 @@ def remove_lock(muxname):
     except:
         return
 
+
 def set_last_used(muxname):
+    t=int(time.time())
     lockfile=lockdir+"/"+muxname+".last_used"
     f=open(lockfile, "w")
-    f.write(str(time.time()))
+    f.write(str(t))
     f.close()
 
-def get_last_used_age(muxname):
+def get_last_used(muxname):
     lockfile=lockdir+"/"+muxname+".last_used"
     if not os.path.exists(lockfile):
-        return 1e10
+        return 0
     f=open(lockfile, "r")
     t=f.read()
     f.close()
     try:
-        return time.time()-float(t)
+        return float(t)
     except:
         os.remove(lockfile)
         return 1e10
@@ -216,15 +218,76 @@ def pos_to_num(pos):
         return -float(s)
     return 0
 
+def update_last_updates():
+    global muxes
+    log_start("Update last updates cnt="+str(len(muxes)))
+    cnt=0
+    for mux in muxes:
+        oldest_service=1e100
+        for text in mux["text_services"]:
+            sdate=get_last_used(text[0])
+            if (sdate<oldest_service):
+                oldest_service=sdate
+        muxdate=get_last_used(mux["uuid"])
+        if (muxdate>oldest_service):
+            #Mux was recieved after oldest service
+            last_update=muxdate
+        else:
+            #services were received later than the mux
+            last_update=oldest_service
+        muxes[muxes.index(mux)]["last_update"]=last_update
+        cnt=cnt+1
+    log_end(str(cnt)+" updates")
+
+def format_last_used(last_used):
+    if last_used==0:
+        return "never"
+    return str(format_delta(time.time()-last_used))+" ago"
+
+
+def format_delta(delta):
+    a=abs(delta) 
+    if a>60:
+        return str(datetime.timedelta(seconds=int(a)))
+    if a>1:
+        return "{:0.5}".format(delta)+"s"
+    if a>0.001:
+        return "{:0.5}".format(delta*1000)+"ms"
+    return "{:0.5}".format(delta*1000000)+"µs"
+
+
+log_time_stack=[]
+
+def log_indent():
+    global log_time_stack
+    l=len(log_time_stack)
+    return "│ "*l
+
+def log_start(text):
+    global log_time_stack
+    indent=log_indent()
+    log_time_stack.append(time.time())
+    print(indent+"┌"+text)
+
+def log(text):
+    global log_time_stack
+    indent=log_indent()
+    print(indent+" "+text)
+
+def log_end(text):
+    global log_time_stack
+    d=time.time()-log_time_stack.pop()
+    indent=log_indent()
+    if text=="":
+        text="done"
+    print(indent+"└▶"+format_delta(d)+" "+text);
+
+    
+
 base_url="http://"+tvheadend_ip+":"+tvheadend_port+"/"
 base_url_auth="http://"+tvheadend_user+":"+tvheadend_pass+"@"+tvheadend_ip+":"+tvheadend_port+"/"
 
-try:
-    with open('blockpids.json') as t_file:
-        blockpids=json.load(t_file)
-except:
-    blockpids={}
-
+log_start("Loading muxes from tvheadend");
 url=base_url+"api/raw/export?class=dvb_mux"
 req=requests.get(url, auth=HTTPDigestAuth(tvheadend_user, tvheadend_pass))
 req.encoding="UTF-8"
@@ -234,15 +297,37 @@ if req.status_code != 200:
     exit()
 
 muxes=json.loads(req.text)
+log_end(str(len(muxes))+" muxes loaded")
 
-filtered_mux_list=[]
 
+log_start("loading and hashing services")
+log_start("Loading services from tvheadend");
+url=base_url+"api/raw/export?class=service"
+req=requests.get(url, auth=HTTPDigestAuth(tvheadend_user, tvheadend_pass))
+req.encoding="UTF-8"
+
+if req.status_code != 200:
+    print("Couldn't get service list. Maybe user has insufficient rights. Code: ", req.status_code)
+    exit()
+
+services=json.loads(req.text)
+log_end(str(len(services))+" muxes")
+log_start("moving to hashmap")
+service_hash={}
+for service in services:
+    muxname=service["uuid"]
+    service_hash[muxname]=service
+log_end("")
+log_end("")
+
+#annotate_and_filter_muxes
+mux_filtered=[]
+load_translations()
+log_start("filter through "+str(len(muxes))+ " multiplexes")
+total_services=0
+disappeared_services=0
+services_left=0
 for mux in muxes:
-    mux_uuid=mux["uuid"]
-    if not use_mux(mux):
-        continue
-    if len(mux["services"])<1:
-           continue
     mux_name=""
     if "delsys" in mux:
         mux_name=mux_name+mux["delsys"]+"-"
@@ -252,119 +337,161 @@ for mux in muxes:
         mux_name=mux_name+mux["polarisation"]
     if "orbital" in mux:
         mux_name=mux_name+"-"+mux["orbital"]
-    age=get_last_used_age(mux_uuid)
-    position=mux["delsys"]
-    if "orbital" in mux:
         position=mux["orbital"]
-    if orbital is not None:
-        if not position in orbital.split(","):
-            continue
-    filtered_mux_list.append([mux_uuid,age,position,mux_name])
+    mux["mux_name"]=mux_name
+    if not "scan_last" in mux:
+        continue
+    mux_scan_last=mux["scan_last"]
+    scnt=0
+    text_services=[]
+    pid_list={}
+    if "services" in mux and len(mux["services"])>0:
+        for suuid in mux["services"]:
+            total_services=total_services+1
+            service=service_hash[suuid]
+            if not "last_seen" in service:
+                log(mux_name+" Service "+suuid+" never seen -> ignoring");
+                continue
+            sname=suuid
+            if "svcname" in service:
+                sname=service["svcname"]
+            last_seen=service["last_seen"]
+            if last_seen+3600<mux_scan_last and last_seen!=0:
+                disappeared_services=disappeared_services+1
+                continue
+            last_seen_age=int(time.time())-last_seen
+            if last_seen_age>(24*5)*3600:
+                disappeared_services=disappeared_services+1
+                continue 
+            tcnt=0;
+            tstreams=[]
+            for stream in service["stream"]:
+                if stream['type']=="TELETEXT":
+                    if stream["pid"] in pid_list:
+                        continue
+                    tstreams.append(stream["pid"])
+                    tcnt=tcnt+1
+            if len(tstreams)>0:
+                sname=service["uuid"]
+                if ('sid' in service):
+                    sname="SID-"+str(service['sid'])
+                if ('svcname' in service):
+                    sname=service['svcname']
+                osrvname=sname.upper().replace(" HD","").replace(" ","").replace("/","").replace("$","").replace(":","_")
+                #Look up service name
+                srvname=translate(osrvname,position,mux_name)
+                if srvname!="BLOCK":
+                    for x in tstreams:
+                        text_services.append([srvname,x])
+                        pid_list[x]=srvname
+                    scnt=scnt+1
+                services_left=services_left+1
+    if len(text_services)>0:
+        mux["text_services"]=text_services
+        #log(mux_name+str(mux["text_services"]))
+        mux_filtered.append(mux)
+save_translations()
+muxes=mux_filtered
+log_end(str(len(muxes))+" muxes left; "+str(services_left)+" out of "+str(total_services)+" services remaining; "+str(disappeared_services)+" services disappeared")
 
-temp_mux_list=sorted(filtered_mux_list, key=lambda d:d[1],reverse=True)
-if sort_sat!=0:
-    sorted_mux_list=sorted(temp_mux_list, key=lambda d:pos_to_num(d[2]))
+
+
+if statusfile is None:
+    sfile=""
 else:
-    sorted_mux_list=temp_mux_list
+    sfile="-s"+statusfile
 
+while len(muxes)>0:
 
-all_mux_pids={}
-for fmux in sorted_mux_list:
-    fmuxname=fmux[0]
-    mux_name=fmux[3]
-    print("Multiplex:", fmuxname, "age:", str(datetime.timedelta(seconds=int(fmux[1]))), "position:", fmux[2], "Friendy-Name:", mux_name)
-    clean_locks()
+    update_last_updates()
 
-    #Check for lock
-    if no_stream==0:
-        if not get_lock(fmuxname):
-            print("Couldn't get lock")
-            continue
-    req=requests.get(base_url+"api/raw/export?uuid="+fmuxname, auth=HTTPDigestAuth(tvheadend_user, tvheadend_pass))
-    mux=json.loads(req.text)[0]
-    mux_uuid=mux["uuid"]
-    if mux_uuid!=fmuxname:
-        print("Didn't return correct mux", mux_uuid, fmuxname)
-        exit()
-    
-    load_translations()
-    mux_pids=[] #Used to build service list in the end and to find the names for the outgoing directories
-    pids=[] #Used to determine PIDs to ask tvheadend for
-    for service in mux['services']:
-        req=requests.get(base_url+"api/raw/export?uuid="+service, auth=HTTPDigestAuth(tvheadend_user, tvheadend_pass))
-        channel=json.loads(req.text)
-        sname=service
-        if ('sid' in channel[0]):
-            sname="SID-"+str(channel[0]['sid'])
-        if ('svcname' in channel[0]):
-            sname=channel[0]['svcname']
-        osrvname=sname.upper().replace(" HD","").replace(" ","").replace("/","").replace("$","").replace(":","_")
-        #Look up service name
-        srvname=translate(osrvname,fmux[2],mux_name)
-        #Skip if service is set to "BLOCK"
-        if srvname=="BLOCK":
-            continue
-        text_stream_cnt=0
-        for stream in channel[0]['stream']:
-            if stream['type']=="TELETEXT":
-                #Skip streams that are in blockpids
-                if mux_uuid in blockpids:
-                    if stream['pid'] in blockpids[mux_uuid]:
-                        continue
-                if mux_name in blockpids:
-                    if stream['pid'] in blockpids[mux_name]:
-                        continue
-                #Only add stream if it hasn't existed before
-                if not stream['pid'] in pids:
-                    pids.append(stream['pid'])
-                    mux_pids.append([srvname,stream['pid'],osrvname]);
-                    text_stream_cnt=text_stream_cnt+1
-        if text_stream_cnt==0:
-            delete_translation(osrvname,fmux[2],mux_name)
+    temp_mux_list=sorted(muxes, key=lambda d:d["last_update"])
+    if sort_sat!=0:
+        muxes=sorted(temp_mux_list, key=lambda d:pos_to_num(d[2]))
+    else:
+        muxes=temp_mux_list
 
-    save_translations()
-    
-    if len(pids)>0:
-        all_mux_pids[mux_name]=mux_pids
-        url=base_url_auth+"stream/mux/"+mux_uuid+"?pids="+",".join(map(str,pids))
-        print(url, no_stream)
-        if no_stream == 0:
-            out_tmp=tmpdir+"/"+mux_uuid
-            os.makedirs(out_tmp, exist_ok=True)
-            date_prefix=datetime.datetime.now().utcnow().isoformat(timespec="seconds")+"+00:00"
-            sfile=""
-            if not statusfile is None:
-                sfile="-s"+statusfile
-            for ch in mux_pids:
-                print(ch[0]+": 0x"+"{:04x}".format(ch[1])+" ("+str(ch[1])+") "+ch[2])
-            os.system("timeout 7200 wget -o /dev/null -O - --read-timeout=20 "+url+" | "+ts_teletext+" --ts --stop "+sfile+"  -p"+out_tmp+"/"+date_prefix+"-")
-            set_last_used(mux_uuid)
+    muxes_to_remove=[]
+
+    log_start("processing "+str(len(muxes))+" muxes")
+    for mux in muxes:
+        time.sleep(1)
+        mux_uuid=mux["uuid"]
+        log_start(mux["mux_name"]+" last update: "+format_last_used(mux["last_update"]))
+        if no_stream==0:
+            if not get_lock(mux_uuid):
+                log_end("Could not get lock for "+mux["mux_name"]+" ("+mux_uuid+")")
+                continue
+        log_start("listing text services of mux "+mux["mux_name"])
+        maxlen=0 
+        for text_service in mux["text_services"]:
+            l=len(text_service[0])
+            if l>maxlen:
+                maxlen=l;
+        for text_service in mux["text_services"]:
+            name=text_service[0]
+            l=len(name)
+            spaces=maxlen+2-l
+            pid=text_service[1]
+            log("  "+name+("…"*spaces)+("{:4}".format(pid))+" 0x"+("{:04x}".format(pid))+ " last update: "+format_last_used(get_last_used(name)))
+        log_end("")
+        out_tmp=tmpdir+"/"+mux_uuid
+        os.makedirs(out_tmp, exist_ok=True)
+        date_prefix=datetime.datetime.now().utcnow().isoformat(timespec="seconds")+"+00:00"
+        pids=[]
+        for text_service in mux["text_services"]:
+            pids.append(text_service[1])
+        spids=",".join(map(str,pids))
+        url=base_url_auth+"stream/mux/"+mux["uuid"]+"?pids=0,1,15,16,17,18,"+spids
+        log_start("Handling multiplex streaming url: "+url)
+        filecount=0
+        if no_stream==0:
+            log_start("Starting to stream from mux")
+            line_indent=log_indent()+ " "
+            os.system("timeout 7200 wget -o /dev/null -O - --read-timeout=20 "+url+" | "+ts_teletext+" --ts --stop "+sfile+" '-P"+line_indent+"' -p"+out_tmp+"/"+date_prefix+"-")
+            log_end("")
+            #Sort files
+            log_start("sort files")
             files=os.listdir(out_tmp)
-            for service in mux_pids:
-                name=service[0]
-                pid=service[1]
+            for text_service in mux["text_services"]:
+                name=text_service[0]
+                pid=text_service[1]
                 pid_suffix="-0x"+"{:04x}".format(pid)+".zip"
                 for f in files:
                     if f.endswith(pid_suffix):
                         os.makedirs(outdir+"/"+name, exist_ok=True)
+                        log(f+" => "+outdir+"/"+name+"/"+name+"/"+f)
                         shutil.move(out_tmp+"/"+f, outdir+"/"+name+"/"+f)
                         files.remove(f)
-    remove_lock(mux_uuid)
-    if len(pids)>0: #Move files to rync target
-        if not rsync_target is None:
-            cmd="rsync -rv "
-            if rsync_remove!=0:
-                cmd=cmd+" --remove-source-files "
-            cmd=cmd+outdir+"/* "+rsync_target
-            print(cmd)
-            os.system(cmd)
-    with open('all_mux_pids.json','w') as t_file:
-        json.dump(all_mux_pids,fp=t_file,indent=4, sort_keys=True)
-    if len(pids)>0:
-        if not limit is None:
-            limit=limit-1
-            if limit <= 0:
-                print("Ran into limit, exiting", limit)
-                exit()
-
+                        set_last_used(name)
+                        filecount=filecount+1
+            log_end(str(filecount)+" files moved")
+            remove_lock(mux["uuid"])
+        if filecount>0:
+            set_last_used(mux["uuid"])
+            if not limit is None:
+                limit=limit-1
+                if limit <= 0:
+                    print("Ran into limit, exiting", limit)
+                    exit()
+        log_end("")
+        muxes_to_remove.append(mux)
+        log_end("")
+        if filecount>0:
+            if not rsync_target is None:
+                log_start("rsync to "+rsync_target)
+                cmd="rsync -rv "
+                if rsync_remove!=0:
+                    cmd=cmd+" --remove-source-files "
+                cmd=cmd+outdir+"/* "+rsync_target
+                print(cmd)
+                os.system(cmd)
+                log_end("")
+            break
+    log_end("")
+    log_start("remove used muxes")
+    for mux in muxes_to_remove:
+        log(mux["mux_name"])
+        muxes.remove(mux)
+    log_end("")
 
